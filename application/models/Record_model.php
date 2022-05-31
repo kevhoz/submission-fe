fbc6065c99f9d2aa663ab3f9fadfe5854a07ac19
Kevin Christianto <kevin.hikoza@gmail.com>
2022-05-31 21:52:30 +0700

    update url

--- application/models/Record_model.php
+++ application/models/Record_model.php
@@ -8,7 +8,7 @@ class Record_model extends CI_Model {
     public function __construct() {
         $this->client = new Client([
             // TODO: Tambahkan Base URL API
-            'base_uri' => "34.101.162.156 ",
+            'base_uri' => "34.101.83.134",
         ]);
     }
 
