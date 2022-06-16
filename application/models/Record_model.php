04cd54a518ee1ef28ecf30557ffe3eb0a9f16d53
Kevin Christianto <kevin.hikoza@gmail.com>
2022-06-16 13:45:58 +0700

    Update link

--- application/models/Record_model.php
+++ application/models/Record_model.php
@@ -8,7 +8,7 @@ class Record_model extends CI_Model {
     public function __construct() {
         $this->client = new Client([
             // TODO: Tambahkan Base URL API
-            'base_uri' => "http://34.128.75.83/",
+            'base_uri' => "http://34.128.124.244/",
         ]);
     }
 
